b640d5fe75d77bf030784d5080d900c720baeebc
Ethan <ethanling1@gmail.com>
2021-02-20 17:17:24 -0500

    Added app data mamagement context

--- src/components/Dock/index.js
+++ src/components/Dock/index.js
@@ -3,8 +3,9 @@ import { AppsDataContext } from '../../context/AppsDataContext'
 import DockIcon from "./DockIcon";
 
 export default function Dock() {
-    const apps = useContext(AppsDataContext)
+    const [apps, dispatch]= useContext(AppsDataContext)
     const iconSize = 3;
+
     return (
         <div className="absolute flex flex-row items-center justify-center w-auto p-4 overflow-x-auto bg-gray-400 shadow-xl ring-1 ring-black ring-opacity-5 bg-opacity-60 space-x-4 bottom-4 rounded-2xl">
             {apps.map((app, i) => {
--- src/context/AppsDataContext.js
+++ src/context/AppsDataContext.js
@@ -1,40 +1,15 @@
-import { useState, useEffect, createContext } from "react";
+import { createContext, useReducer } from "react";
+// Array of objects represetning all available apps with metadata
 import { apps } from "../apps";
+import { reducer } from './AppDataReducer'
 
 export const AppsDataContext = createContext(apps);
 
 export const AppsDataProvider = ({ children }) => {
-  const [appData, setAppData] = useState(apps);
 
-  const launchApp = (name) => {
-    // Check if app exists
-    const app = appData.filter((item, index) => item.name === name)[0]
-    console.log("Initial App State:", app)
-    const exists = app ? true : false;
-    const isRunning = app.running;
-
-    if (exists && !isRunning) {
-      // Update application state
-      app.running = true
-      console.log(app.name, app.running)
-      // Push update to app array
-      //setAppData(prevState => {
-        //prevstate
-      //})
-    }
-
-    // Copy state obj
-    // update state object
-    // set as state
-  }
-
-  useEffect(() => {
-    launchApp('Settings')
-  }, [])
-
-  return (
-    <AppsDataContext.Provider value={appData}>
-      {children}
-    </AppsDataContext.Provider>
-  );
+    return (
+        <AppsDataContext.Provider value={useReducer(reducer, apps)}>
+            {children}
+        </AppsDataContext.Provider>
+    );
 };
